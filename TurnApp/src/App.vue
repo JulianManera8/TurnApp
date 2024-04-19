2d762246a08c401b2363a147f03bf778e70c927d
JulianManera8 <julianmanera@hotmail.com>
2024-04-18 22:07:34 -0300

    listo

---
+++ TurnApp/src/App.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <H1>hola</H1>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ TurnApp/src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVueStyled from 'primevue/styled';
+
+
+import App from './App.vue'
+import router from './router'
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVueStyled)
+
+app.mount('#app')
